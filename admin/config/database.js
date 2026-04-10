7084a850dc0b6002485f8d1ff3cd9fda6392d2c3
Israel Anthony <seu-email@gmail.com>
2026-04-10 01:07:57 +0100

    Atualização_1.6.0

--- admin/config/database.js
+++ admin/config/database.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
 
